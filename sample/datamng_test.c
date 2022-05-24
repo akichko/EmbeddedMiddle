074351d8d51bdd59f230ad67d30d0ee3d87facba
akichko <s.akihiko@gmail.com>
2022-05-24 19:37:45 +0900

    sample added

---
+++ sample/datamng_test.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "../src/em_datamng.h"
+
+typedef struct
+{
+	int attr1;
+	int attr2;
+	int attr3;
+} em_test_t;
+
+int main()
+{
+	em_datamng_t dm;
+	em_blockmng_t *block;
+	em_test_t test_data = {0, 0, 0};
+	int ret;
+
+	em_create_datamng(&dm, sizeof(em_test_t), 10);
+	em_print_datamng(&dm);
+
+	printf("add id=1,2,3\n");
+	for (int i = 0; i < 3; i++)
+	{
+		test_data.attr1 = i + 10;
+		em_set_data(&dm, &test_data, i + 1);
+	}
+	em_print_datamng(&dm);
+
+	ret = em_get_block(&dm, 2, &block);
+	printf("get_block ret:%d id=2\n", ret);
+	ret = em_get_block(&dm, 5, &block);
+	printf("get_block ret:%d id=5\n", ret);
+
+	printf("add id=1\n");
+	em_set_data(&dm, &test_data, 1);
+	em_print_datamng(&dm);
+
+	printf("del id=2\n");
+	em_del_block(&dm, 2);
+	em_print_datamng(&dm);
+
+	printf("add id=5\n");
+	em_set_data(&dm, &test_data, 5);
+	em_print_datamng(&dm);
+
+	printf("del id=1\n");
+	em_del_block(&dm, 1);
+	em_print_datamng(&dm);
+
+	em_delete_datamng(&dm);
+}
---
+++ sample/mpool_test.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../src/em_mempool.h"
+
+int main()
+{
+	em_mpool_t mp;
+	
+	int *idata[10];
+	em_create_mpool(&mp, 10, sizeof(int));
+	em_print_mpool(&mp);
+
+	printf("alloc\n");
+	for (int i = 0; i < 5; i++)
+	{
+		em_alloc_block(&mp, (void **)&idata[i]);
+		*idata[i] = i + 1;
+	}
+	em_print_mpool(&mp);
+
+	// printf("free\n");
+	em_free_block(&mp, idata[0]);
+	em_print_mpool(&mp);
+	em_free_block(&mp, idata[2]);
+	em_print_mpool(&mp);
+
+	printf("alloc2\n");
+	for (int i = 5; i < 10; i++)
+	{
+		em_alloc_block(&mp, (void **)&idata[i]);
+		*idata[i] = i + 1;
+		em_print_mpool(&mp);
+	}
+	em_free_block(&mp, idata[5]);
+	em_print_mpool(&mp);
+	em_free_block(&mp, idata[9]);
+	em_print_mpool(&mp);
+	em_alloc_block(&mp, (void **)&idata[0]);
+	*idata[0] = 11;
+	em_print_mpool(&mp);
+
+	
+	em_delete_mpool(&mp);
+}
